d23dca58fbd23e24e6d884e56999a880eaab4009
Andreas Grafen <andreas@grafen.info>
2021-02-02 23:16:54 +0100

    run build

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,107 +2,112 @@
 
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-var cleanStuff = function cleanStuff(obj) {
-  // clean main checks // weird, but useful for security reason
-  var safObj = {},
-      // will need help (clean more the string to avoid escaped characters)
-  cleanString = new RegExp(/"|'/, 'g'),
-      bugs = [];
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-  try {
-    // names
-    var keys = Object.keys(obj); // object
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-    if (_typeof(obj) !== 'object') bugs.push('Only objects allowed.');
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-    if (keys.length === 4) {
-      if (keys[0] !== 'title' || typeof obj[keys[0]] !== 'string' || keys[1] !== 'link' || typeof obj[keys[1]] !== 'string' || keys[2] !== 'description' || typeof obj[keys[2]] !== 'string' || keys[3] !== 'image' || typeof obj[keys[3]] !== 'string') bugs.push('Verify name of object keys, respectively, it must be: title, link, description, image. All must be strings types without escaped characters.');else {
-        var props = keys.map(function (k) {
-          return obj[k];
-        });
-        props = props.map(function (w) {
-          return w.replace(cleanString, '');
-        }); // parse already
+var Card = /*#__PURE__*/function () {
+  function Card(data, id) {
+    _classCallCheck(this, Card);
 
-        var _ref = [props[0], props[1], props[2], props[3]];
-        safObj.title = _ref[0];
-        safObj.link = _ref[1];
-        safObj.description = _ref[2];
-        safObj.image = _ref[3];
-      }
-    } else bugs.push('Only 4 object keys allowed.'); // all bugs
+    this._id = id + 1;
+    this._themeIsValid = this.validateTheme(data);
 
-
-    if (bugs.length !== 0) throw bugs.length;
-  } catch (e) {
-    console.log("Theme called ".concat(obj.title, " has the following bug(s), please resolve (").concat(e, "):\n").concat(bugs.join('\n')));
-    return null;
-  } // need to verify image manually
-
-
-  return safObj;
-}; // count each card to parse 
-
-
-fetch('themes.json').then(function (data) {
-  return data.json();
-}).then(function (parsedData) {
-  var i = 0;
-  parsedData.forEach(function (entry) {
-    var cleanObj = cleanStuff(entry),
-        id = "card-".concat(i);
-    i++; //console.log(safeObj.title);
-
-    if (cleanObj !== null) {
-      var _ref2 = [cleanObj.title, cleanObj.link, cleanObj.image, cleanObj.description],
-          title = _ref2[0],
-          link = _ref2[1],
-          image = _ref2[2],
-          description = _ref2[3];
-      var elemns = "\n      <div class=\"card\" id=\"".concat(id, "\">\n        <header>\n          <h3 class=\"theme-title\">\n            <a></a>\n          </h3>\n          <i class=\"fas fa-chevron-circle-down\"></i>\n        </header>\n        <a class=\"meta\">\n          <img>\n          <p class=\"description\"></p>\n        </a>\n      </div>\n      ");
-      var container = document.getElementById('main_content');
-      container.insertAdjacentHTML('beforeend', elemns);
-      var card = document.getElementById(id),
-          titlehead = card.getElementsByTagName('header')[0].getElementsByClassName('theme-title')[0],
-          meta = card.getElementsByClassName('meta')[0],
-          img = card.getElementsByTagName('img')[0]; // 100% security
-
-      meta.href = titlehead.href = link;
-      img.alt = title;
-      titlehead.getElementsByTagName('a')[0].innerText = title;
-      img.src = image;
-      meta.getElementsByClassName('description')[0].innerText = description;
+    if (this._themeIsValid) {
+      this._title = this.sanatise(data.title);
+      this._description = this.sanatise(data.description);
+      this._link = this.sanatise(data.link);
+      this._image = this.sanatise(data.image);
     }
+  }
+
+  _createClass(Card, [{
+    key: "validateTheme",
+    value: function validateTheme(data) {
+      var bugs = [];
+
+      try {
+        var keys = Object.keys(data);
+        var objectKeys = keys[0] !== 'title' || keys[1] !== 'link' || keys[2] !== 'description' || keys[3] !== 'image';
+        var objectTypes = typeof data[keys[0]] !== 'string' || typeof data[keys[1]] !== 'string' || typeof data[keys[2]] !== 'string' || typeof data[keys[3]] !== 'string';
+        if (_typeof(data) !== 'object') bugs.push('This theme is not an object.');
+        if (keys.length !== 4) bugs.push("Is expected to have 4 key but has ".concat(keys.length, "."));
+        if (objectKeys) bugs.push('Verify name of object keys, respectively, it must be: title, link, description, image.');
+        if (objectTypes) bugs.push('All object entries must be strings!');
+        if (bugs.length !== 0) throw bugs.length;
+      } catch (errorCount) {
+        console.warn("Theme ".concat(this._id, " has the following ").concat(errorCount, " bug(s):\n").concat(bugs.join('\n')));
+        return false;
+      }
+
+      return true;
+    }
+  }, {
+    key: "sanatise",
+    value: function sanatise(unsanatisedInput) {
+      var tempEl = document.createElement('div');
+      tempEl.innerText = unsanatisedInput;
+      var sanatisedOutput = tempEl.innerHTML;
+      return sanatisedOutput;
+    }
+  }, {
+    key: "render",
+    value: function render(outputContainer) {
+      // don't render anything when the theme format is invalid
+      if (!this._themeIsValid) return;
+      var template = "\n    <div id=\"theme-".concat(this._id, "\" class=\"card\">\n      <header>\n        <h3 class=\"theme-title\"><a hef=\"").concat(this._link, "\">").concat(this._title, "</a></h3>\n        <i class=\"fas fa-chevron-circle-down\"></i>\n      </header>\n      <a class=\"meta\" href=\"").concat(this._link, "\">\n        <img src=\"").concat(this._image, "\">\n        <p class=\"description\">").concat(this._description, "</p>\n      </a>\n    </div>\n    ");
+      outputContainer.insertAdjacentHTML('beforeend', template);
+    }
+  }]);
+
+  return Card;
+}();
+
+(function () {
+  // IIFE to avoid globals
+
+  /*  Load Content
+   *  ============
+   */
+  fetch('themes.json').then(function (data) {
+    return data.json();
+  }).then(function (parsedData) {
+    parsedData.forEach(function (entry, index) {
+      var outputContainer = document.getElementById('main_content');
+      var card = new Card(entry, index);
+      card.render(outputContainer);
+    });
   });
-}); // Themes
+  /*  Theme Handling
+   *  ==============
+   */
 
-var prefDark = window.matchMedia("(prefers-color-scheme: dark)").matches,
-    prefTheme = localStorage['theme'],
-    themeTrigger = document.getElementById('js-themeSwitcher'),
-    themeTriggerIcon = themeTrigger.querySelector('i');
+  var systemPref = window.matchMedia("(prefers-color-scheme: dark)").matches ? 'night' : 'day',
+      themeTrigger = document.getElementById('js-themeSwitcher'),
+      themeTriggerIcon = themeTrigger.querySelector('i'); // when local storage is not populated set the system preferrence as value
 
-var toggleTheme = function toggleTheme() {
-  document.documentElement.classList.toggle('nightmode');
-  document.documentElement.classList.toggle('daymode');
-  themeTriggerIcon.classList.toggle('fa-sun');
-  themeTriggerIcon.classList.toggle('fa-moon');
-};
+  if (!localStorage['theme']) localStorage['theme'] = systemPref === 'day' ? 'day' : 'night'; // set nightmode when according to local storage
 
-if (prefDark) {
-  document.documentElement.classList.add('nightmode');
-  themeTriggerIcon.classList.remove('fa-moon');
-  themeTriggerIcon.classList.add('fa-sun');
-}
+  if (localStorage['theme'] === 'night') {
+    themeTriggerIcon.classList.toggle('fa-sun');
+    themeTriggerIcon.classList.toggle('fa-moon');
+    document.documentElement.classList.add('nightmode');
+  } else {
+    document.documentElement.classList.add('daymode');
+  }
 
-if (!prefDark) {
-  document.body.classList.add('daymode');
-}
+  function toggleTheme() {
+    document.documentElement.classList.toggle('nightmode');
+    document.documentElement.classList.toggle('daymode');
+    themeTriggerIcon.classList.toggle('fa-sun');
+    themeTriggerIcon.classList.toggle('fa-moon'); // update local storage
 
-if (prefTheme === 'day') {
-  toggleTheme();
-} else localStorage['theme'] = 'night';
+    if (localStorage['theme'] === 'night') localStorage['theme'] = 'day';else localStorage['theme'] = 'night';
+  }
 
-themeTrigger.addEventListener('click', function (event) {
-  if (localStorage['theme'] === 'night') localStorage['theme'] = 'day';else localStorage['theme'] = 'night';
-  toggleTheme();
-});
+  themeTrigger.addEventListener('click', function (event) {
+    return toggleTheme();
+  });
+})();
