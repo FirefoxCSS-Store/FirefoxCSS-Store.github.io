7b2998ab5840b7e91f16a0f37828a356c3890143
Andreas <andreas@grafen.info>
2024-01-28 18:16:08 +0100

    fix(build): asset path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,9 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  outDir: './docs'
+  outDir: './docs',
+  build: {
+    assets: 'assets',
+    assetsPrefix: '.'
+  }
 })
