aa992818ba549bf10fafa14d5787d6d4755c9fe6
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-10-11 04:11:34 -0300

    fix(tests): allow theme to have more fields and accept repository field

--- tests/themes.check.js
+++ tests/themes.check.js
@@ -11,15 +11,17 @@ themes.forEach((entry, index) => {
                       keys[1] !== 'link'        ||
                       keys[2] !== 'description' ||
                       keys[3] !== 'image'       ||
-                      keys[4] !== 'tags'
+                      keys[4] !== 'tags'        ||
+                      keys[5] !== 'repository'
   const objectTypes = typeof entry[keys[0]] !== 'string' ||
                       typeof entry[keys[1]] !== 'string' ||
                       typeof entry[keys[2]] !== 'string' ||
                       typeof entry[keys[3]] !== 'string' ||
-                      typeof entry[keys[4]] !== 'object'
+                      typeof entry[keys[4]] !== 'object' !!
+                      typeof entry[keys[5]] !== 'string'
 
   if (typeof entry !== 'object') currentBugs.push('This theme is not an object.')
-  if (keys.length !== 5) currentBugs.push(`Is expected to have 5 key but has ${keys.length}.`)
+  if (keys.length < 6) currentBugs.push(`Is expected to have at least 6 keys, but has ${keys.length}.`)
   if (objectKeys) currentBugs.push('Verify name of object keys, respectively, it must be: title, link, description, image, tags')
   if (objectTypes) currentBugs.push('All object entries must be strings with the exception of tags, which is an array!')
 
